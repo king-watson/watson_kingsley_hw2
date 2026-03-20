7a93e2eea896e34b3ed0bc032807996f64553fd8
king-watson <watsonkingsley38@gmail.com>
2026-03-20 04:05:11 -0400

    gsap. scrollto in main.js because modules wasnt working

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 import './modules/hamburger.js';
+import './modules/animations.js';
 import { Counter, StepCounter } from './modules/counter.js';
+
+const navLinks = document.querySelectorAll("#menu a, #desktopMenu a");
+
+function handleNavClick(e) {
+    e.preventDefault();
+    const target = e.currentTarget.getAttribute("href");
+    gsap.to(window, {
+        duration: 1,
+        scrollTo: target,
+        ease: "power2.inOut"
+    });
+}
+
+navLinks.forEach(function addScrollListener(link) {
+    link.addEventListener("click", handleNavClick);
+});
 
 const counter1 = new Counter("#counter-container1", 10);
 const counter2 = new Counter("#counter-container2");
